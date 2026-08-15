bc75c4063c102cecfe626e7d9acc8e2aca25a0d4
perdanamain <firman.fp123@gmail.com>
2026-08-15 13:23:25 +0700

    feat: initialize new Strapi project structure with base API and configuration files

---
+++ src/api/founder-setting/controllers/founder-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * founder-setting controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::founder-setting.founder-setting');
---
+++ src/api/founder-setting/services/founder-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * founder-setting service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::founder-setting.founder-setting');
