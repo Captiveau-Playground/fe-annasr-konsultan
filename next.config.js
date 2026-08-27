7a2579b0c71be6b83700ea1f8576dc82529e0a09
perdanamain <firman.fp123@gmail.com>
2026-08-27 14:30:43 +0700

    chore: configure standalone output mode, include swc helpers, and allow all remote image domains

--- next.config.js
+++ next.config.js
@@ -1,16 +1,30 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-            },
-            {
-                protocol: "http",
-                hostname: "127.0.0.1",
-            },
-        ],
-    },
+  output: "standalone",
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@swc/helpers/**/*",
+      "./node_modules/.pnpm/**/node_modules/@swc/helpers/**/*"
+    ],
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
-export default nextConfig;
+
+module.exports = nextConfig;
+
